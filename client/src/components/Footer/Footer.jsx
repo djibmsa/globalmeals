c0913ca0560e11ff389ed7888b2706d6c5c43200
djibmsa@gmail.com <djibmsa@gmail.com>
2024-10-02 11:32:19 +0200

    Styling GitHub and LinkedIn links in the footer.

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,12 +1,36 @@
+// Footer.js
+
 import "./Footer.css";
+// Importation des icônes depuis react-icons
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer>
       <section className="footer">
-        <p>Copyright © 2024</p>
+      <p>Copyright © 2024</p>
+        <div className="social-icons">
+        
+          <a
+            href="https://www.linkedin.com/in/zakarya-moussa-boina-b71a56191"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/djibmsa"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <FaGithub />
+          </a>
+        </div>
       </section>
     </footer>
   );
 }
+
 export default Footer;
